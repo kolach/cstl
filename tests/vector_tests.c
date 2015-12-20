990d68bda8e8f81078f9649c893665a98f3c6046
kolach <chikolad@gmail.com>
2015-12-20 17:21:56 -0600

    template for void* vector tests

---
+++ tests/vector_tests.c
@@ -0,0 +1,20 @@
+#include "minunit.h"
+
+void before_all()   {}
+void after_all()    {}
+void before_each()  {}
+void after_each()   {}
+
+char* test_create() {
+  return NULL;
+}
+
+char *all_tests() {
+  mu_suite_start(before_each, after_each);
+  before_all();
+  mu_run_test(test_create);
+  after_all();
+  return NULL;
+}
+
+RUN_TESTS(all_tests);
